e4551ce1862ef0164e828b64bd7d9a6eeed435c1
mmmmr <ob12564@163.com>
2023-10-26 22:46:10 +0800

    ???

--- components.d.ts
+++ components.d.ts
@@ -6,23 +6,23 @@
 export {}
 
 declare module 'vue' {
-  export interface GlobalComponents {
-    AButton: typeof import('ant-design-vue/es')['Button']
-    ACol: typeof import('ant-design-vue/es')['Col']
-    AForm: typeof import('ant-design-vue/es')['Form']
-    AFormItem: typeof import('ant-design-vue/es')['FormItem']
-    AInput: typeof import('ant-design-vue/es')['Input']
-    AInputPassword: typeof import('ant-design-vue/es')['InputPassword']
-    AMenu: typeof import('ant-design-vue/es')['Menu']
-    AMenuItem: typeof import('ant-design-vue/es')['MenuItem']
-    AMenuItemGroup: typeof import('ant-design-vue/es')['MenuItemGroup']
-    ARow: typeof import('ant-design-vue/es')['Row']
-    ASpin: typeof import('ant-design-vue/es')['Spin']
-    ASubMenu: typeof import('ant-design-vue/es')['SubMenu']
-    Form: typeof import('./src/components/atom/form/index.vue')['default']
-    FormItem: typeof import('./src/components/atom/form/components/formItem.vue')['default']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
-    Table: typeof import('./src/components/atom/table/index.vue')['default']
-  }
+	export interface GlobalComponents {
+		AButton: (typeof import('ant-design-vue/es'))['Button']
+		ACol: (typeof import('ant-design-vue/es'))['Col']
+		AForm: (typeof import('ant-design-vue/es'))['Form']
+		AFormItem: (typeof import('ant-design-vue/es'))['FormItem']
+		AInput: (typeof import('ant-design-vue/es'))['Input']
+		AInputPassword: (typeof import('ant-design-vue/es'))['InputPassword']
+		AMenu: (typeof import('ant-design-vue/es'))['Menu']
+		AMenuItem: (typeof import('ant-design-vue/es'))['MenuItem']
+		AMenuItemGroup: (typeof import('ant-design-vue/es'))['MenuItemGroup']
+		ARow: (typeof import('ant-design-vue/es'))['Row']
+		ASpin: (typeof import('ant-design-vue/es'))['Spin']
+		ASubMenu: (typeof import('ant-design-vue/es'))['SubMenu']
+		Form: (typeof import('./src/components/atom/form/index.vue'))['default']
+		FormItem: (typeof import('./src/components/atom/form/components/formItem.vue'))['default']
+		RouterLink: (typeof import('vue-router'))['RouterLink']
+		RouterView: (typeof import('vue-router'))['RouterView']
+		Table: (typeof import('./src/components/atom/table/index.vue'))['default']
+	}
 }
