56d2cfb9e9dac6232a40d78b843f2a4c033771e9
bsuooo <91053541+bsuooo@users.noreply.github.com>
2024-09-05 17:10:29 +0800

    Delete src/components/atom/form directory

--- src/components/atom/form/types.ts
+++
@@ -1,6 +0,0 @@
-export type FormSpan = number | 'full' | 'half' | 'third' | 'quarter'
-export type AnyObject = Record<string, any>
-export interface ProvideVForm {
-  form: AnyObject
-  span: number
-}
